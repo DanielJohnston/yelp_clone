32d3b18afbed7f483a2f7988948b4f3ca8ba0289
DanielJohnston <macademy@danieljohnston.co.uk>
2015-06-16 11:20:51 +0100

    restaurant delete tests passing

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -44,7 +44,7 @@ feature 'restaurants' do
       end
     end
 
-    context 'editing restaurants' do 
+    context 'editing restaurants' do
 
       before {Restaurant.create name: 'KFC'}
 
@@ -56,5 +56,18 @@ feature 'restaurants' do
         expect(page).to have_content 'Kentucky Fried Chicken'
         expect(current_path).to eq '/restaurants'
       end
+    end
+
+    context 'deleting restaurants' do
+
+      before {Restaurant.create name: 'KFC'}
+
+      scenario 'let a user delete a restaurant' do
+        visit '/restaurants'
+        click_link 'Delete KFC'
+        expect(page).not_to have_content 'KFC'
+        expect(page).to have_content 'Restaurant deleted successfully'
+      end
+
     end
 end
